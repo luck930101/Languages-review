3e9408bf3e577089c323e42be7755dc81e4980ac
Yubo Zhang <clannad93@gmail.com>
2018-01-30 18:37:48 -0800

    Operater

---
+++ Java/Operators/OperatorReview.java
@@ -0,0 +1,21 @@
+
+public class OperatorReview {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1 = 82;
+		System.out.println("num1%9 is : "+num1%9);// print 1;
+		System.out.println("num1/9 is : "+num1/9);// print 9
+		
+		double num2 = 82.9999;
+		System.out.println("num2%9 is : "+num2%9);
+		System.out.println("num2/9 is : "+num2/9);
+		
+		int i = 0;
+		int j = 0;
+		System.out.println("i++ is : "+ i++);
+		System.out.println("++j is : "+ j++);
+	}
+
+}
